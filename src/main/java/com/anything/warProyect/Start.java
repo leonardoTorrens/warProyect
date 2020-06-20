e3eee7ad2ba724dc6b36b16e7c68935e97e79dab
Matias Nicolas Garcia <matias.89lp@gmail.com>
2020-06-20 00:24:04 -0300

    rename main class

--- src/main/java/com/anything/warProyect/Start.java
+++ src/main/java/com/anything/warProyect/Start.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WarProyectApplication {
+public class Start {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WarProyectApplication.class, args);
+		SpringApplication.run(Start.class, args);
 	}
 
 }
