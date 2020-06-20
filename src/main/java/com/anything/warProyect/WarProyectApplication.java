5f53a7ab53b5d7b484ffa6c0e1b6ee241b7d0c94
Matias Nicolas Garcia <matias.89lp@gmail.com>
2020-06-20 00:06:51 -0300

    Agregar estructura de back a la app

---
+++ src/main/java/com/anything/warProyect/WarProyectApplication.java
@@ -0,0 +1,13 @@
+package com.anything.warProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarProyectApplication.class, args);
+	}
+
+}
