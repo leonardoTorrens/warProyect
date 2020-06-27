6295fbe275a82042997613faca9d343f0b152d44
Matias Nicolas Garcia <matias.89lp@gmail.com>
2020-06-27 15:58:31 -0300

    version 1 of back structure

--- src/main/java/com/anything/warProyect/Start.java
+++ src/main/java/com/anything/warProyect/Start.java
@@ -1,13 +1,18 @@
 package com.anything.warProyect;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Start {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(Start.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Start.class, args);
+		LOGGER.info("warProyect is up");
 	}
 
 }
